76b08d92a1828d91e6b2ca11b3464e397292adaa
stevenhvtran <stransteven@outlook.com>
2018-06-25 21:16:22 +1000

    Fixed SQLAlchemy models

--- forum_app/db.py
+++ forum_app/db.py
@@ -6,7 +6,8 @@ db = SQLAlchemy()
 class User(db.Model):
     __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
+    username = db.Column(db.Text, unique=True, nullable=False)
+    password = db.Column(db.Text, nullable=False)
     posts = db.relationship('Post', backref='user')
 
     def __repr__(self):
@@ -16,8 +17,8 @@ class User(db.Model):
 class Post(db.Model):
     __tablename__ = 'post'
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String, nullable=False)
-    body = db.Column(db.String)
+    title = db.Column(db.Text, nullable=False)
+    body = db.Column(db.Text)
     author_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
     def __repr__(self):

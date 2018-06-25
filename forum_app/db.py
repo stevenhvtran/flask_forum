4b059af009b5f654d06cdae2579d4ef1d861f086
stevenhvtran <stransteven@outlook.com>
2018-06-25 18:06:41 +1000

    Added database models for users and posts

---
+++ forum_app/db.py
@@ -0,0 +1,24 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+
+class User(db.Model):
+    __tablename__ = 'user'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    posts = db.relationship('Post', backref='user')
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+
+class Post(db.Model):
+    __tablename__ = 'post'
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String, nullable=False)
+    body = db.Column(db.String)
+    author_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __repr__(self):
+        return '<Post %r>' % self.title

0d9083ef1f0abdfadd0fadcf2b2fe950a1be591d
stevenhvtran <stransteven@outlook.com>
2018-06-25 18:06:19 +1000

    Database now starts with empty tables when app is run

--- forum_app/__init__.py
+++ forum_app/__init__.py
@@ -1,16 +1,19 @@
 from flask import Flask
-from flask.json import jsonify
 
 
 def create_app():
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_envvar('FLASK_CONFIG')
 
-    from forum_app.model import db
+    from forum_app.db import db
     db.init_app(app)
 
-    @app.route('/')
-    def hello():
-        return jsonify('Hello World')
+    with app.test_request_context():
+        db.drop_all()  # resets the database when server is first run
+        db.create_all()  # creates all the tables from db.py
+
+    from forum_app import forum
+    app.register_blueprint(forum.bp)
+    app.add_url_rule('/', endpoint='index')
 
     return app

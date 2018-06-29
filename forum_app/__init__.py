bae988875943ceae9c980ed22a9f8e851ab2b142
stevenhvtran <stransteven@outlook.com>
2018-06-29 16:16:20 +1000

    Removed un-needed argument in application factory

--- forum_app/__init__.py
+++ forum_app/__init__.py
@@ -2,13 +2,13 @@ from flask import Flask
 
 
 def create_app():
-    app = Flask(__name__, instance_relative_config=True)
+    app = Flask(__name__)
     app.config.from_envvar('FLASK_CONFIG')
 
     from forum_app.models.db import db
     db.init_app(app)
     with app.test_request_context():
-        db.drop_all()  # drops all tables in testdb
+        db.drop_all()  # drops all tables in database
         db.create_all()  # creates all the tables from db models
 
         from forum_app import forum

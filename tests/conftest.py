da00cc4554a6ab333e872fb9677f92c4e2ae4f60
stevenhvtran <stransteven@outlook.com>
2018-06-29 10:42:22 +1000

    Added test cases for flask forum

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,7 @@
 import pytest
 from forum_app import create_app
 from forum_app.models.db import db
+import base64
 
 
 @pytest.fixture
@@ -34,5 +35,30 @@ def client_with_user():
                              'username': 'test123',
                              'password': 'test123'
                          })
+
+        yield test_client
+
+
+@pytest.fixture
+def client_with_post():
+    app = create_app()
+    app.config['TESTING'] = True
+    test_client = app.test_client()
+
+    db.init_app(app)
+    with app.test_request_context():
+        db.drop_all()  # drops all tables for fresh database
+        db.create_all()  # creates all the tables from db models
+
+        test_client.post('/api/register',
+                         json={
+                             'username': 'test123',
+                             'password': 'test123'
+                         })
+
+        valid_credentials = base64.b64encode(b'test123:test123').decode('utf-8')
+        test_client.post('/api/submit',
+                         json={'title': 'test title', 'body': 'test body'},
+                         headers={'Authorization': 'Basic ' + valid_credentials})
 
         yield test_client
---
+++ tests/test_forum.py
@@ -0,0 +1,86 @@
+import pytest
+import base64
+
+
+@pytest.mark.usefixtures('client_with_user')
+def test_index(client_with_user):
+    valid_credentials = base64.b64encode(b'test123:test123').decode('utf-8')
+    response = client_with_user.get('/', headers={'Authorization': 'Basic ' + valid_credentials})
+    assert response.status_code == 200
+    assert dict(response.get_json()) == {'message': 'Hello test123'}
+
+
+@pytest.mark.usefixtures('client')
+def test_get_all_posts_unpopulated(client):
+    response = client.get('/api/posts')
+    assert response.status_code == 200
+    assert dict(response.get_json()) == {'posts': []}
+
+
+@pytest.mark.usefixtures('client_with_post')
+def test_get_all_posts_populated(client_with_post):
+    response = client_with_post.get('/api/posts')
+    expected_post_dict = {
+        'post_id': 1,
+        'title': 'test title',
+        'body': 'test body',
+        'author_id': 1,
+        'author_name': 'test123',
+        'url': '/api/post/1'
+    }
+    assert response.status_code == 200
+    assert dict(response.get_json()) == {'posts': [expected_post_dict]}
+
+
+@pytest.mark.usefixtures('client')
+def test_get_post_non_existent(client):
+    response = client.get('/api/post/1')
+    assert response.status_code == 404
+    assert dict(response.get_json()) == {'error': 'Post not found'}
+
+
+@pytest.mark.usefixtures('client_with_post')
+def test_get_post_exists(client_with_post):
+    response = client_with_post.get('/api/post/1')
+    expected_post_dict = {
+        'post_id': 1,
+        'title': 'test title',
+        'body': 'test body',
+        'author_id': 1,
+        'author_name': 'test123',
+        'url': '/api/post/1'
+    }
+    assert response.status_code == 200
+    assert dict(response.get_json()) == expected_post_dict
+
+
+@pytest.mark.usefixtures('client_with_user')
+def test_submit_post(client_with_user):
+    valid_credentials = base64.b64encode(b'test123:test123').decode('utf-8')
+    response = client_with_user.post('/api/submit',
+                                     json={'title': 'test title', 'body': 'test body'},
+                                     headers={'Authorization': 'Basic ' + valid_credentials})
+    assert response.status_code == 200
+    assert dict(response.get_json()) == {'message': 'Post created successfully'}
+
+
+@pytest.mark.usefixtures('client_with_user')
+@pytest.mark.parametrize('title', [1, 'a', 'supersupersupersuperlongtitle', True])
+def test_submit_post_title_error(client_with_user, title):
+    valid_credentials = base64.b64encode(b'test123:test123').decode('utf-8')
+    response = client_with_user.post('/api/submit',
+                                     json={'title': title, 'body': 'test body'},
+                                     headers={'Authorization': 'Basic ' + valid_credentials})
+    assert response.status_code == 200
+    assert dict(response.get_json()) == {'error': 'Invalid title'}
+
+
+@pytest.mark.usefixtures('client_with_user')
+@pytest.mark.parametrize('body', [1, True, ['some list']])
+def test_submit_post_body_error(client_with_user, body):
+    valid_credentials = base64.b64encode(b'test123:test123').decode('utf-8')
+    response = client_with_user.post('/api/submit',
+                                     json={'title': 'test title', 'body': body},
+                                     headers={'Authorization': 'Basic ' + valid_credentials})
+    assert response.status_code == 200
+    assert dict(response.get_json()) == {'error': 'Invalid body'}

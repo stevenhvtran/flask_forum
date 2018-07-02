ba3ef4c66b8b220236937c25406223b2c9abf1f1
stevenhvtran <stransteven@outlook.com>
2018-07-02 10:45:56 +1000

    Refactored helper_fns and forum
    Moved error messages closer to where they would be called and removed
    duplicate code as much as possible

--- forum_app/forum.py
+++ forum_app/forum.py
@@ -21,10 +21,22 @@ def get_all_posts():
 
 @bp.route('/post/<int:post_id>', methods=('GET',))
 def get_post(post_id):
-    post = get_post_from_post_id(post_id)
+    post = Find.post(Post.id, post_id)
     if post:
         return jsonify(post_to_dict(post))
     return make_response(jsonify({'error': 'Post not found'}), 404)
+
+
+def get_post_errors():
+    response = get_post_response()
+    if response:
+        title, body = response
+        if not ValidPost.title(title):
+            return jsonify({'error': 'Invalid title'})
+        if not ValidPost.body(body):
+            return jsonify({'error': 'Invalid body'})
+        return None
+    return make_response({'error': 'Malformed request'}, 400)
 
 
 @bp.route('/submit', methods=('POST',))
@@ -32,25 +44,27 @@ def get_post(post_id):
 def submit_post():
     errors = get_post_errors()
     if errors:
-        return jsonify(errors)
-    commit_post_to_db()
+        return errors
+    PostTable.add()
     return jsonify({'message': 'Post created successfully'})
 
 
 @bp.route('/post/<int:post_id>', methods=('PUT', 'DELETE'))
 @auth.login_required
 def modify_post(post_id):
-    update_errors = get_post_update_errors(post_id)
-    if update_errors:
-        return make_response(jsonify(update_errors['error_msg']), update_errors['status_code'])
+    post = Find.post(Post.id, post_id)
+    if not post:
+        return make_response(jsonify({'error': 'Post not found'}), 404)
+    if post.author_id != Find.user(User.username, auth.username()).id:
+        return make_response(jsonify({'error': 'You do not have permission to edit this post'}), 401)
 
     if request.method == 'PUT':
         errors = get_post_errors()
         if errors:
-            return jsonify(errors)
-        update_post_in_db(post_id)
+            return errors
+        PostTable.update(post_id)
         return jsonify({'message': 'Post updated successfully'})
 
     if request.method == 'DELETE':
-        delete_post(post_id)
+        PostTable.delete(post_id)
         return jsonify({'message': 'Post deleted successfully'})
--- forum_app/helper_fns.py
+++ forum_app/helper_fns.py
@@ -3,19 +3,10 @@ from forum_app.models.auth import auth
 from forum_app.models.db import db, Post, User
 
 
-def get_user_from_id(user_id):
-    user = User.query.filter(User.id == user_id).first()
-    return user
-
-
-def get_post_from_post_id(post_id):
-    post = Post.query.filter(Post.id == post_id).first()
-    return post
-
-
 def post_to_dict(post_obj):
+    """Converts Post object to a dictionary of values"""
     if isinstance(post_obj, Post):
-        author = get_user_from_id(post_obj.author_id)
+        author = Find.user(User.id, post_obj.author_id)
         post_dict = {
             'post_id': post_obj.id,
             'title': post_obj.title,
@@ -28,67 +19,62 @@ def post_to_dict(post_obj):
     return None
 
 
-def get_user_id(username):
-    user = User.query.filter(User.username == username).first()
-    if user:
-        return user.id
-    return None
-
-
 def get_post_response():
     response = request.get_json()
-    return response['title'], response['body']
-
-
-def commit_post_to_db():
-    title, body = get_post_response()
-    author_id = get_user_id(auth.username())
-    post = Post(title=title, body=body, author_id=author_id)
-    db.session.add(post)
-    db.session.commit()
-
-
-def valid_title(title):
-    if isinstance(title, str):
-        if 3 < len(title) < 25:
-            return True
-    return False
-
-
-def valid_body(body):
-    if isinstance(body, str):
-        if len(body) <= 1000:
-            return True
-    return False
-
-
-def get_post_errors():
-    title, body = get_post_response()
-
-    if not valid_title(title):
-        return {'error': 'Invalid title'}
-    if not valid_body(body):
-        return {'error': 'Invalid body'}
+    if response:
+        if 'title' in response.keys():
+            if 'body' in response.keys():
+                return response['title'], response['body']
+            return response['title'], ''
     return None
 
 
-def update_post_in_db(post_id):
-    post = get_post_from_post_id(post_id)
-    title, body = get_post_response()
-    post.title, post.body = title, body
-    db.session.commit()
+class Find:
+    @staticmethod
+    def user(column, value):
+        user = User.query.filter(column == value).first()
+        return user
+
+    @staticmethod
+    def post(column, value):
+        post = Post.query.filter(column == value).first()
+        return post
 
 
-def get_post_update_errors(post_id):
-    post = get_post_from_post_id(post_id)
-    if not post:
-        return dict(error_msg={'error': 'Post not found'}, status_code=404)
-    if post.author_id != get_user_id(auth.username()):
-        return dict(error_msg={'error': 'You do not have permission to edit this post'}, status_code=401)
-    return None
+class PostTable:
+    @staticmethod
+    def add():
+        title, body = get_post_response()
+        author_id = Find.user(User.username, auth.username()).id
+        post = Post(title=title, body=body, author_id=author_id)
+        db.session.add(post)
+        db.session.commit()
+
+    @staticmethod
+    def update(post_id):
+        post = Find.post(Post.id, post_id)
+        title, body = get_post_response()
+        post.title, post.body = title, body
+        db.session.commit()
+
+    @staticmethod
+    def delete(post_id):
+        post = Find.post(Post.id, post_id)
+        db.session.delete(post)
+        db.session.commit()
 
 
-def delete_post(post_id):
-    post = get_post_from_post_id(post_id)
-    db.session.delete(post)
-    db.session.commit()
+class ValidPost:
+    @staticmethod
+    def title(title):
+        if isinstance(title, str):
+            if 3 < len(title) < 25:
+                return True
+        return False
+
+    @staticmethod
+    def body(body):
+        if isinstance(body, str):
+            if len(body) <= 1000:
+                return True
+        return False
